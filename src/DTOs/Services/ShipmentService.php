0cfff0bfe303d6f320a980ce0fe02f00e7c6b64a
xGrz <grzesiek.byskiniewicz@gmail.com>
2025-08-18 16:29:24 +0200

    [feat] ShipmentService added

---
+++ src/DTOs/Services/ShipmentService.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Xgrz\InPost\DTOs\Services;
+
+use Xgrz\InPost\Exceptions\ShipXException;
+use Xgrz\InPost\Models\InPostService;
+
+class ShipmentService
+{
+    protected array $customAttributes = [];
+    protected array $additionalServices = [];
+    protected ?InPostService $service = NULL;
+
+    public function __construct()
+    {
+    }
+
+    public static function make(): static
+    {
+        return new static();
+    }
+
+    /**
+     * @throws ShipXException
+     */
+    public function setService(string $serviceName): static
+    {
+        $service = InPostService::query()
+            ->with('additionalServices')
+            ->where('id', $serviceName)
+            ->first();
+
+        if ($service && $service->active) {
+            $this->service = $service;
+            $this->additionalServices = [];
+        } elseif (! $service) {
+            throw new ShipXException('Service not found: [' . $serviceName . ']');
+        } else {
+            throw new ShipXException('Service [' . $serviceName . '] is locally disabled');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @throws ShipXException
+     */
+    public function additionalService(string $additionalServiceName): static
+    {
+        if (! $this->service) {
+            throw new ShipXException('Service not set. Use setService() method first.');
+        }
+
+        $additionalService = $this->service
+            ->additionalServices
+            ->filter(fn($additionalService) => $additionalService->ident === str($additionalServiceName)->lower()->toString())
+            ->first();
+
+        if ($additionalService && $additionalService->active) {
+            $this->additionalServices[] = $additionalService->ident;
+        } elseif (! $additionalService) {
+            throw new ShipXException('Additional service not found [' . $additionalServiceName . ']');
+        } else {
+            throw new ShipXException('Additional service [' . $additionalServiceName . '] is locally disabled for selected ' . $this->service->id . ' service');
+        }
+
+        return $this;
+    }
+
+    public function targetPoint(?string $targetPoint): static
+    {
+        if ($targetPoint) {
+            $this->customAttributes['target_point'] = $targetPoint;
+        }
+
+        return $this;
+    }
+
+//    public function sendingMethod(?string $sendingMethod): static
+//    {
+//        // todo: verify is this needed
+//        if ($sendingMethod) {
+//            $this->customAttributes['sending_method'] = $sendingMethod;
+//        }
+//
+//        return $this;
+//    }
+
+    public function payload(array $basePayload = []): array
+    {
+        $basePayload['service'] = $this->service?->id;
+        $basePayload['additional_services'] = $this->additionalServices;
+        $basePayload['custom_attributes'] = $this->customAttributes;
+        return $basePayload;
+    }
+}
--- src/Facades/InPostShipment.php
+++ src/Facades/InPostShipment.php
@@ -7,30 +7,23 @@ use Xgrz\InPost\DTOs\Address\Sender;
 use Xgrz\InPost\DTOs\CashOnDelivery\CashOnDelivery;
 use Xgrz\InPost\DTOs\Insurance\Insurance;
 use Xgrz\InPost\DTOs\Parcels\Parcels;
+use Xgrz\InPost\DTOs\Services\ShipmentService;
 
 class InPostShipment
 {
     public ?Sender $sender = NULL; // if not provided, will be set default InPost data
     public Receiver $receiver;
     public Parcels $parcels;
-
-    // custom_attributes
-
     public CashOnDelivery $cash_on_delivery;
-
     public Insurance $insurance;
+    public ShipmentService $service;
 
-    // reference
+    public ?string $reference = NULL;
+    public bool $is_return = false;
+    public ?string $mpk = NULL; // todo: fix this; -> add check if mpk exists
+    public ?string $comments = NULL;
 
-    // is_return
-
-    // service
-
-    // additional_services
-
-    // mpk
-
-    // comments
+    public bool $only_choice_of_offer = false;
 
     public function __construct()
     {
@@ -38,16 +31,22 @@ class InPostShipment
         $this->parcels = Parcels::make();
         $this->cash_on_delivery = new CashOnDelivery();
         $this->insurance = new Insurance();
+        $this->service = ShipmentService::make();
     }
 
     public function payload(): array
     {
         return [
-            'sender' => $this->sender?->payload(),
-            'receiver' => $this->receiver->payload(),
-            'parcels' => $this->parcels->payload(),
-            'cod' => $this->cash_on_delivery->payload(),
-            'insurance' => $this->insurance->payload(),
-        ];
+                'sender' => $this->sender?->payload(),
+                'receiver' => $this->receiver->payload(),
+                'parcels' => $this->parcels->payload(),
+                'cod' => $this->cash_on_delivery->payload(),
+                'insurance' => $this->insurance->payload(),
+                'only_choice_of_offer' => $this->only_choice_of_offer,
+                'reference' => $this->reference,
+                'is_return' => $this->is_return,
+                'mpk' => $this->mpk,
+                'comments' => $this->comments,
+            ] + $this->service->payload();
     }
 }
